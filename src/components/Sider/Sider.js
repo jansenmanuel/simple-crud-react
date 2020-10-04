47be025c0da8ad5dfe1cbb3633295e569dc393e6
jansenmanuel <jansen.manuel@nexsoft.co.id>
2020-10-04 18:15:46 +0700

    add calendar, carousel, fix icon

--- src/components/Sider/Sider.js
+++ src/components/Sider/Sider.js
@@ -2,10 +2,6 @@ import React from "react";
 import "antd/dist/antd.css";
 import "./Sider.css";
 import { Layout, Menu } from "antd";
-import {
-    DashboardOutlined,
-    UserOutlined
-} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
 const { Sider } = Layout;
@@ -26,11 +22,29 @@ class MySider extends React.Component {
             <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                 <div className="logo" />
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                    <Menu.Item key="1" icon={<DashboardOutlined />}>
-                        <Link to="/">Dashboard</Link>
+                    <Menu.Item key="1">
+                        <Link to="/">
+                            <div className="row">
+                                <div className="col-2">
+                                    <i className="fa fa-tachometer" />
+                                </div>
+                                <div className="col-10">
+                                    <p>Dashboard</p>
+                                </div>
+                            </div>
+                        </Link>
                     </Menu.Item>
-                    <Menu.Item key="2" icon={<UserOutlined />}>
-                        <Link to="/siswa">Siswa</Link>
+                    <Menu.Item key="2" >
+                        <Link to="/siswa">
+                            <div className="row">
+                                <div className="col-2">
+                                    <i className="fa fa-user" />
+                                </div>
+                                <div className="col-10">
+                                    <p>Siswa</p>
+                                </div>
+                            </div>
+                        </Link>
                     </Menu.Item>
                 </Menu>
             </Sider>
--- src/container/pages/Dashboard/Dashboard.js
+++ src/container/pages/Dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import "antd/dist/antd.css";
 import "./Dashboard.css";
 import { Layout } from "antd";
+import MyCarousel from "../../../components/Carousel/Carousel";
+import MyCalendar from "../../../components/Calendar/Calendar";
 const { Content } = Layout;
 
 class Dashboard extends React.Component {
@@ -15,7 +17,17 @@ class Dashboard extends React.Component {
                     minHeight: 280
                 }}
             >
-                Content
+                <h3>Dashboard</h3>
+                <div className="row">
+                    <div className="col-6">
+                        <h5>Carousel</h5>
+                        <MyCarousel />
+                    </div>
+                    <div className="col-6">
+                        <h5>Calendar</h5>
+                        <MyCalendar />
+                    </div>
+                </div>
             </Content>
         );
     }
